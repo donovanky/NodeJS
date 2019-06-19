0249d16e6dad0d26acbfabe33df9fc3023a271c5
donovanky <donovanky@my.easternct.edu>
2019-06-18 22:29:04 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,20 @@
+const http = require ('http');
+const port = 3000;
+
+const requestHandler = (request, response) => {
+  response.end('Handling a request on port ${port}')
+};
+
+const server = http.createServer(requestHandler);
+
+
+
+sever.listen(port, (err) => {
+  if (err){
+    return console.log('You have an error: ${err}');
+  }
+  console.log({'Server is listening on ${port}'});
+});
+
+
+fs.writeFileSync('hello-world.txt', 'Hello to this great world', 'utf8');
